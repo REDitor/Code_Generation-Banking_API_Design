42553d5be6e5fa7b8cb41299e940bd6eb459437e
brumarq <brunocm@pm.me>
2022-05-16 18:59:26 +0200

    missing files

---
+++ src/main/java/io/swagger/model/TransactionDepositDTO.java
@@ -0,0 +1,201 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.UUID;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * TransactionDepositDTO
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-05-16T16:44:39.788Z[GMT]")
+
+
+public class TransactionDepositDTO   {
+  @JsonProperty("transactionId")
+  private UUID transactionId = null;
+
+  @JsonProperty("timestamp")
+  private String timestamp = null;
+
+  @JsonProperty("From")
+  private String from = null;
+
+  @JsonProperty("To")
+  private String to = null;
+
+  @JsonProperty("Amount")
+  private Double amount = null;
+
+  @JsonProperty("PerformedByID")
+  private Integer performedByID = null;
+
+  public TransactionDepositDTO transactionId(UUID transactionId) {
+    this.transactionId = transactionId;
+    return this;
+  }
+
+  /**
+   * Get transactionId
+   * @return transactionId
+   **/
+  @Schema(description = "")
+  
+    @Valid
+    public UUID getTransactionId() {
+    return transactionId;
+  }
+
+  public void setTransactionId(UUID transactionId) {
+    this.transactionId = transactionId;
+  }
+
+  public TransactionDepositDTO timestamp(String timestamp) {
+    this.timestamp = timestamp;
+    return this;
+  }
+
+  /**
+   * Date of birth
+   * @return timestamp
+   **/
+  @Schema(example = "2021-03-20T09:12:28Z", description = "Date of birth")
+  
+    public String getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(String timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  public TransactionDepositDTO from(String from) {
+    this.from = from;
+    return this;
+  }
+
+  /**
+   * Get from
+   * @return from
+   **/
+  @Schema(description = "")
+  
+  @Size(min=18,max=18)   public String getFrom() {
+    return from;
+  }
+
+  public void setFrom(String from) {
+    this.from = from;
+  }
+
+  public TransactionDepositDTO to(String to) {
+    this.to = to;
+    return this;
+  }
+
+  /**
+   * Get to
+   * @return to
+   **/
+  @Schema(example = "NL01INHO0000000002", description = "")
+  
+  @Size(min=18,max=18)   public String getTo() {
+    return to;
+  }
+
+  public void setTo(String to) {
+    this.to = to;
+  }
+
+  public TransactionDepositDTO amount(Double amount) {
+    this.amount = amount;
+    return this;
+  }
+
+  /**
+   * The amount to transfer
+   * @return amount
+   **/
+  @Schema(example = "11.23", description = "The amount to transfer")
+  
+    public Double getAmount() {
+    return amount;
+  }
+
+  public void setAmount(Double amount) {
+    this.amount = amount;
+  }
+
+  public TransactionDepositDTO performedByID(Integer performedByID) {
+    this.performedByID = performedByID;
+    return this;
+  }
+
+  /**
+   * Get performedByID
+   * @return performedByID
+   **/
+  @Schema(example = "1", description = "")
+  
+    public Integer getPerformedByID() {
+    return performedByID;
+  }
+
+  public void setPerformedByID(Integer performedByID) {
+    this.performedByID = performedByID;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    TransactionDepositDTO transactionDepositDTO = (TransactionDepositDTO) o;
+    return Objects.equals(this.transactionId, transactionDepositDTO.transactionId) &&
+        Objects.equals(this.timestamp, transactionDepositDTO.timestamp) &&
+        Objects.equals(this.from, transactionDepositDTO.from) &&
+        Objects.equals(this.to, transactionDepositDTO.to) &&
+        Objects.equals(this.amount, transactionDepositDTO.amount) &&
+        Objects.equals(this.performedByID, transactionDepositDTO.performedByID);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(transactionId, timestamp, from, to, amount, performedByID);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class TransactionDepositDTO {\n");
+    
+    sb.append("    transactionId: ").append(toIndentedString(transactionId)).append("\n");
+    sb.append("    timestamp: ").append(toIndentedString(timestamp)).append("\n");
+    sb.append("    from: ").append(toIndentedString(from)).append("\n");
+    sb.append("    to: ").append(toIndentedString(to)).append("\n");
+    sb.append("    amount: ").append(toIndentedString(amount)).append("\n");
+    sb.append("    performedByID: ").append(toIndentedString(performedByID)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
---
+++ src/main/java/io/swagger/model/UserWithAccountsDTO.java
@@ -0,0 +1,357 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.model.AccountDTO;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.ArrayList;
+import java.util.List;
+import org.threeten.bp.LocalDate;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * UserWithAccountsDTO
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-05-16T16:44:39.788Z[GMT]")
+
+
+public class UserWithAccountsDTO   {
+  @JsonProperty("CustomerId")
+  private Integer customerId = null;
+
+  @JsonProperty("FirstName")
+  private String firstName = null;
+
+  @JsonProperty("LastName")
+  private String lastName = null;
+
+  @JsonProperty("BirthDate")
+  private LocalDate birthDate = null;
+
+  @JsonProperty("StreetName")
+  private String streetName = null;
+
+  @JsonProperty("HouseNumber")
+  private Integer houseNumber = null;
+
+  @JsonProperty("ZipCode")
+  private String zipCode = null;
+
+  @JsonProperty("City")
+  private String city = null;
+
+  @JsonProperty("Country")
+  private String country = null;
+
+  @JsonProperty("TransactionAmountLimit")
+  private Integer transactionAmountLimit = null;
+
+  @JsonProperty("DailyLimit")
+  private Integer dailyLimit = null;
+
+  @JsonProperty("Accounts")
+  @Valid
+  private List<AccountDTO> accounts = null;
+
+  public UserWithAccountsDTO customerId(Integer customerId) {
+    this.customerId = customerId;
+    return this;
+  }
+
+  /**
+   * Get customerId
+   * @return customerId
+   **/
+  @Schema(example = "1", description = "")
+  
+    public Integer getCustomerId() {
+    return customerId;
+  }
+
+  public void setCustomerId(Integer customerId) {
+    this.customerId = customerId;
+  }
+
+  public UserWithAccountsDTO firstName(String firstName) {
+    this.firstName = firstName;
+    return this;
+  }
+
+  /**
+   * Get firstName
+   * @return firstName
+   **/
+  @Schema(example = "Bruno", description = "")
+  
+    public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public UserWithAccountsDTO lastName(String lastName) {
+    this.lastName = lastName;
+    return this;
+  }
+
+  /**
+   * Get lastName
+   * @return lastName
+   **/
+  @Schema(example = "Coimbra Marques", description = "")
+  
+    public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public UserWithAccountsDTO birthDate(LocalDate birthDate) {
+    this.birthDate = birthDate;
+    return this;
+  }
+
+  /**
+   * Date of birth
+   * @return birthDate
+   **/
+  @Schema(example = "Tue Oct 12 00:00:00 GMT 1999", description = "Date of birth")
+  
+    @Valid
+    public LocalDate getBirthDate() {
+    return birthDate;
+  }
+
+  public void setBirthDate(LocalDate birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  public UserWithAccountsDTO streetName(String streetName) {
+    this.streetName = streetName;
+    return this;
+  }
+
+  /**
+   * Get streetName
+   * @return streetName
+   **/
+  @Schema(example = "Pietersbergweg", description = "")
+  
+    public String getStreetName() {
+    return streetName;
+  }
+
+  public void setStreetName(String streetName) {
+    this.streetName = streetName;
+  }
+
+  public UserWithAccountsDTO houseNumber(Integer houseNumber) {
+    this.houseNumber = houseNumber;
+    return this;
+  }
+
+  /**
+   * Get houseNumber
+   * @return houseNumber
+   **/
+  @Schema(example = "1234", description = "")
+  
+    public Integer getHouseNumber() {
+    return houseNumber;
+  }
+
+  public void setHouseNumber(Integer houseNumber) {
+    this.houseNumber = houseNumber;
+  }
+
+  public UserWithAccountsDTO zipCode(String zipCode) {
+    this.zipCode = zipCode;
+    return this;
+  }
+
+  /**
+   * Get zipCode
+   * @return zipCode
+   **/
+  @Schema(example = "0987 MB", description = "")
+  
+    public String getZipCode() {
+    return zipCode;
+  }
+
+  public void setZipCode(String zipCode) {
+    this.zipCode = zipCode;
+  }
+
+  public UserWithAccountsDTO city(String city) {
+    this.city = city;
+    return this;
+  }
+
+  /**
+   * Get city
+   * @return city
+   **/
+  @Schema(example = "Amsterdam", description = "")
+  
+    public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public UserWithAccountsDTO country(String country) {
+    this.country = country;
+    return this;
+  }
+
+  /**
+   * Get country
+   * @return country
+   **/
+  @Schema(example = "Netherlands", description = "")
+  
+    public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  public UserWithAccountsDTO transactionAmountLimit(Integer transactionAmountLimit) {
+    this.transactionAmountLimit = transactionAmountLimit;
+    return this;
+  }
+
+  /**
+   * Get transactionAmountLimit
+   * @return transactionAmountLimit
+   **/
+  @Schema(example = "2000", description = "")
+  
+    public Integer getTransactionAmountLimit() {
+    return transactionAmountLimit;
+  }
+
+  public void setTransactionAmountLimit(Integer transactionAmountLimit) {
+    this.transactionAmountLimit = transactionAmountLimit;
+  }
+
+  public UserWithAccountsDTO dailyLimit(Integer dailyLimit) {
+    this.dailyLimit = dailyLimit;
+    return this;
+  }
+
+  /**
+   * Get dailyLimit
+   * @return dailyLimit
+   **/
+  @Schema(example = "500", description = "")
+  
+    public Integer getDailyLimit() {
+    return dailyLimit;
+  }
+
+  public void setDailyLimit(Integer dailyLimit) {
+    this.dailyLimit = dailyLimit;
+  }
+
+  public UserWithAccountsDTO accounts(List<AccountDTO> accounts) {
+    this.accounts = accounts;
+    return this;
+  }
+
+  public UserWithAccountsDTO addAccountsItem(AccountDTO accountsItem) {
+    if (this.accounts == null) {
+      this.accounts = new ArrayList<AccountDTO>();
+    }
+    this.accounts.add(accountsItem);
+    return this;
+  }
+
+  /**
+   * Get accounts
+   * @return accounts
+   **/
+  @Schema(description = "")
+      @Valid
+    public List<AccountDTO> getAccounts() {
+    return accounts;
+  }
+
+  public void setAccounts(List<AccountDTO> accounts) {
+    this.accounts = accounts;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    UserWithAccountsDTO userWithAccountsDTO = (UserWithAccountsDTO) o;
+    return Objects.equals(this.customerId, userWithAccountsDTO.customerId) &&
+        Objects.equals(this.firstName, userWithAccountsDTO.firstName) &&
+        Objects.equals(this.lastName, userWithAccountsDTO.lastName) &&
+        Objects.equals(this.birthDate, userWithAccountsDTO.birthDate) &&
+        Objects.equals(this.streetName, userWithAccountsDTO.streetName) &&
+        Objects.equals(this.houseNumber, userWithAccountsDTO.houseNumber) &&
+        Objects.equals(this.zipCode, userWithAccountsDTO.zipCode) &&
+        Objects.equals(this.city, userWithAccountsDTO.city) &&
+        Objects.equals(this.country, userWithAccountsDTO.country) &&
+        Objects.equals(this.transactionAmountLimit, userWithAccountsDTO.transactionAmountLimit) &&
+        Objects.equals(this.dailyLimit, userWithAccountsDTO.dailyLimit) &&
+        Objects.equals(this.accounts, userWithAccountsDTO.accounts);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(customerId, firstName, lastName, birthDate, streetName, houseNumber, zipCode, city, country, transactionAmountLimit, dailyLimit, accounts);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class UserWithAccountsDTO {\n");
+    
+    sb.append("    customerId: ").append(toIndentedString(customerId)).append("\n");
+    sb.append("    firstName: ").append(toIndentedString(firstName)).append("\n");
+    sb.append("    lastName: ").append(toIndentedString(lastName)).append("\n");
+    sb.append("    birthDate: ").append(toIndentedString(birthDate)).append("\n");
+    sb.append("    streetName: ").append(toIndentedString(streetName)).append("\n");
+    sb.append("    houseNumber: ").append(toIndentedString(houseNumber)).append("\n");
+    sb.append("    zipCode: ").append(toIndentedString(zipCode)).append("\n");
+    sb.append("    city: ").append(toIndentedString(city)).append("\n");
+    sb.append("    country: ").append(toIndentedString(country)).append("\n");
+    sb.append("    transactionAmountLimit: ").append(toIndentedString(transactionAmountLimit)).append("\n");
+    sb.append("    dailyLimit: ").append(toIndentedString(dailyLimit)).append("\n");
+    sb.append("    accounts: ").append(toIndentedString(accounts)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
