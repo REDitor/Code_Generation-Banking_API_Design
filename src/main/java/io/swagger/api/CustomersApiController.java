bc4be390e3ccea0e963c74d9d0c25bff104c6b8a
brumarq <brunocm@pm.me>
2022-05-29 00:53:24 +0200

    get all customers

--- src/main/java/io/swagger/api/CustomersApiController.java
+++ src/main/java/io/swagger/api/CustomersApiController.java
@@ -15,9 +15,11 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,10 +35,15 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.validation.constraints.*;
 import javax.validation.Valid;
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-05-17T19:48:55.418Z[GMT]")
 @RestController
@@ -84,8 +91,14 @@ public class CustomersApiController implements CustomersApi {
 )) @Valid @RequestParam(value = "skip", required = false) Integer skip,@Min(0) @Max(50) @Parameter(in = ParameterIn.QUERY, description = "maximum number of records to return" ,schema=@Schema(allowableValues={  }, maximum="50"
 )) @Valid @RequestParam(value = "limit", required = false) Integer limit) {
         // TODO
+        ModelMapper modelMapper = new ModelMapper();
 
-        return new ResponseEntity<List<UserCustomerDTO>>(HttpStatus.NOT_IMPLEMENTED);
+        Pageable page = PageRequest.of(skip, limit);
+
+        List<User> receivedUser = userService.getAll(page);
+        List<UserCustomerDTO> entityToDto = modelMapper.map(receivedUser, new TypeToken<List<UserCustomerDTO>>(){}.getType());
+
+        return new ResponseEntity<List<UserCustomerDTO>>(entityToDto,  HttpStatus.OK);
     }
 
     public ResponseEntity<UserCustomerDTO> updateCustomer(@Parameter(in = ParameterIn.PATH, description = "The customerID of the customer", required=true, schema=@Schema()) @PathVariable("customerId") Integer customerId,@Parameter(in = ParameterIn.QUERY, description = "include list of accounts of selected user" ,schema=@Schema()) @Valid @RequestParam(value = "includeAccountInfo", required = false) Boolean includeAccountInfo,@Parameter(in = ParameterIn.DEFAULT, description = "New customer details", schema=@Schema()) @Valid @RequestBody NewUserCustomerDTO body) {
--- src/main/java/io/swagger/service/UserService.java
+++ src/main/java/io/swagger/service/UserService.java
@@ -5,8 +5,11 @@ import io.swagger.model.entity.User;
 import io.swagger.repository.AccountRepository;
 import io.swagger.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -21,4 +24,11 @@ public class UserService {
     public User getOne(UUID userID) {
         return userRepository.getOne(userID);
     }
+
+    public List<User> getAll(Pageable page) {
+        Page<User> customerPage = userRepository.findAll(page);
+
+        return customerPage.getContent();
+    }
+
 }
