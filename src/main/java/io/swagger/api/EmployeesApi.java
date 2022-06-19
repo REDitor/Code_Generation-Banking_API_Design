d07b6f64c5aba1b61a9618d21c232659004d655e
brumarq <brunocm@pm.me>
2022-06-19 13:46:52 +0200

    changes for CORS

--- src/main/java/io/swagger/api/EmployeesApi.java
+++ src/main/java/io/swagger/api/EmployeesApi.java
@@ -21,11 +21,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.*;
@@ -33,6 +29,7 @@ import java.util.List;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-05-31T12:15:45.146Z[GMT]")
 @Validated
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public interface EmployeesApi {
 
     @Operation(summary = "Creates a new Employee", description = "Create new Employee", security = {
--- src/main/java/io/swagger/api/LoginApi.java
+++ src/main/java/io/swagger/api/LoginApi.java
@@ -17,15 +17,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.bind.annotation.CookieValue;
 
 import javax.validation.Valid;
 import javax.validation.constraints.*;
@@ -34,6 +27,7 @@ import java.util.Map;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-05-30T14:26:03.164Z[GMT]")
 @Validated
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public interface LoginApi {
 
     @Operation(summary = "Login to account", description = "", security = {
