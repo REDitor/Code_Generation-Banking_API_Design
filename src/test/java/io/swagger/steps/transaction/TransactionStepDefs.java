8f42793acda88c39b52884e6b0a4c7f3854877c4
REDitor <sander.harks@outlook.com>
2023-05-27 13:15:45 +0200

    Finished Cucumber tests for transactions & updated POM httpclient

--- src/test/java/io/swagger/steps/transaction/TransactionStepDefs.java
+++ src/test/java/io/swagger/steps/transaction/TransactionStepDefs.java
@@ -7,9 +7,12 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.swagger.model.TransactionDTO;
+import io.swagger.model.entity.Account;
+import io.swagger.repository.AccountRepository;
 import io.swagger.steps.BaseStepDefinitions;
 import io.swagger.steps.CucumberContextConfig;
 import org.junit.jupiter.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
@@ -18,8 +21,8 @@ import org.springframework.http.ResponseEntity;
 @SpringBootTest(classes = CucumberContextConfig.class)
 public class TransactionStepDefs extends BaseStepDefinitions implements En {
 
-    private static final String VALID_TOKEN_USER = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJTYW5kZXJIYXJrczEyMyIsImF1dGgiOlt7ImF1dGhvcml0eSI6IlJPTEVfQ1VTVE9NRVIifV0sImlhdCI6MTY4NTA0MDQ0OCwiZXhwIjoxNjg1MDQ0MDQ4fQ.N3wnqsLQsZst-qMeyVLNDc-9004SXBa7IRqmmUlejas";
-    private static final String VALID_TOKEN_ADMIN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJCcnVub01hcnF1ZXMxMjMiLCJhdXRoIjpbeyJhdXRob3JpdHkiOiJST0xFX0VNUExPWUVFIn1dLCJpYXQiOjE2ODUwNDA0MzAsImV4cCI6MTY4NTA0NDAzMH0.bB6FQFUabBeQBg7jph2G0yh6LTpOWEqBc0aWagrwYZQ";
+    private static final String VALID_TOKEN_USER = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJTYW5kZXJIYXJrczEyMyIsImF1dGgiOlt7ImF1dGhvcml0eSI6IlJPTEVfQ1VTVE9NRVIifV0sImlhdCI6MTY4NTE4NTczMywiZXhwIjoxNjg1MTg5MzMzfQ.sQg219PyXPOIPLpr4bsuDdViyqN72MWl16vexIoo0tI";
+    private static final String VALID_TOKEN_ADMIN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJCcnVub01hcnF1ZXMxMjMiLCJhdXRoIjpbeyJhdXRob3JpdHkiOiJST0xFX0VNUExPWUVFIn1dLCJpYXQiOjE2ODUxODU2ODksImV4cCI6MTY4NTE4OTI4OX0.YQ56vCwg-J2gkogZ1ngfn5lHH3p8UTag6bAUXpZZhUo";
 
     private static final String INVALID_TOKEN = "InvalidToken";
     private final TestRestTemplate restTemplate = new TestRestTemplate();
@@ -29,18 +32,26 @@ public class TransactionStepDefs extends BaseStepDefinitions implements En {
     private Integer status;
 
     private TransactionDTO newTransactionDTO;
-
-    private final String validIBAN = "NL01INHO0000000002";
+    @Autowired
+    private AccountRepository accountRepository;
 
     public TransactionStepDefs() {
-        //region Creating a Transaction
-        // valid
         Given("^I have a valid token for role \"([^\"]*)\" or role \"([^\"]*)\"$", (String roleUser, String roleAdmin) -> {
             if (roleAdmin.equals("admin")) {
                 setHttpHeaders(VALID_TOKEN_ADMIN);
             } else if (roleUser.equals("user")) {
                 setHttpHeaders(VALID_TOKEN_USER);
             }
+        });
+        Given("^I have a valid token for role \"([^\"]*)\"$", (String role) -> {
+            if (role.equals("admin")) {
+                setHttpHeaders(VALID_TOKEN_ADMIN);
+            } else if (role.equals("user")) {
+                setHttpHeaders(VALID_TOKEN_USER);
+            }
+        });
+        Given("^I have an invalid token$", () -> {
+            setHttpHeaders(INVALID_TOKEN);
         });
         And("^I have a transaction object with amount \"([^\"]*)\" and from \"([^\"]*)\" and to \"([^\"]*)\"$", (String amount, String from, String to) -> {
             TransactionDTO transaction = new TransactionDTO();
@@ -58,31 +69,11 @@ public class TransactionStepDefs extends BaseStepDefinitions implements En {
         Then("^I receive status code (\\d+)$", (Integer statusCode) -> {
             Assertions.assertEquals(statusCode, status);
         });
-
-        // invalid token
-        Given("^I have an invalid token for creating a transaction$", () -> {
-            setHttpHeaders(INVALID_TOKEN);
-        });
-        //endregion
-
-        //region Get Transactions
-        // Valid token, IBAN, and date
-        Given("^I have a valid token for role \"([^\"]*)\"$", (String role) -> {
-            if (role.equals("admin")) {
-                setHttpHeaders(VALID_TOKEN_ADMIN);
-            } else if (role.equals("user")) {
-                setHttpHeaders(VALID_TOKEN_USER);
-            }
-        });
-        When("^I call the get all transactions endpoint with iban \"([^\"]*)\" dateTimeFrom \"([^\"]*)\" and dateTimeTo \"([^\"]*)\"$", (String iban, String dateTimeFrom, String dateTimeTo) -> {
+        When("^I call the get all transactions endpoint with iban \"([^\"]*)\" and dateTimeFrom \"([^\"]*)\" and dateTimeTo \"([^\"]*)\"$", (String iban, String dateTimeFrom, String dateTimeTo) -> {
             request = new HttpEntity<>(null, httpHeaders);
             response = restTemplate.exchange(getBaseUrl() + "/transactions/" + iban + "?dateTimeFrom=" + dateTimeFrom + "&dateTimeTo=" + dateTimeTo, HttpMethod.GET, request, String.class);
             status = response.getStatusCodeValue();
         });
-        Given("^I have an invalid token for role \"([^\"]*)\"$", (String roleAdmin) -> {
-            setHttpHeaders(INVALID_TOKEN);
-        });
-
 
         And("^I have a transaction object with amount \"([^\"]*)\" and to \"([^\"]*)\"$", (String amount, String iban) -> {
             TransactionDTO transaction = new TransactionDTO();
@@ -96,7 +87,22 @@ public class TransactionStepDefs extends BaseStepDefinitions implements En {
             response = restTemplate.exchange(getBaseUrl() + "transactions/" + newTransactionDTO.getTo()  + "/deposit", HttpMethod.POST, request, String.class);
             status = response.getStatusCodeValue();
         });
-        //endregion
+        When("^I call the get all transactions endpoint with userId \"([^\"]*)\" and dateTimeFrom \"([^\"]*)\" and dateTimeTo \"([^\"]*)\"$", (String userId, String dateTimeFrom, String dateTimeTo) -> {
+            request = new HttpEntity<>(null, httpHeaders);
+            response = restTemplate.exchange(getBaseUrl() + "/transactions/users/" + userId + "?dateTimeFrom=" + dateTimeFrom + "&dateTimeTo=" + dateTimeTo, HttpMethod.GET, request, String.class);
+            status = response.getStatusCodeValue();
+        });
+        And("^I have a transaction object with amount \"([^\"]*)\" \\(higher or equal to account balance\\) and from \"([^\"]*)\"$", (String amount, String iban) -> {
+            TransactionDTO transaction = new TransactionDTO();
+            transaction.setAmount(Double.parseDouble(amount));
+            transaction.setFrom(iban);
 
+            newTransactionDTO = transaction;
+        });
+        When("^I call the post withdraw endpoint$", () -> {
+            request = new HttpEntity<>(mapper.writeValueAsString(newTransactionDTO), httpHeaders);
+            response = restTemplate.exchange(getBaseUrl() + "transactions/" + newTransactionDTO.getFrom()  + "/withdraw", HttpMethod.POST, request, String.class);
+            status = response.getStatusCodeValue();
+        });
     }
 }
